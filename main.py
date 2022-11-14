4b0aa47d9d1f94d6a196a6ca767ef5814d719cd3
wolflergf <wolflergf@gmail.com>
2022-11-14 23:39:02 +0000

    new files

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.secret_key = "huansijpmpkijinhqwd"
+
+
+# Create a form Class
+class NameForm(FlaskForm):
+    name = StringField("What's your name:", validators=[DataRequired()])
+    submit = SubmitField("Sumit")
 
 
 @app.route("/")
@@ -23,5 +33,20 @@ def page_not_found(e):
     return render_template("500.html"), 500
 
 
+# Create Name Page
+@app.route("/name", methods=["POST", "GET"])
+def name():
+    name = None
+    form = NameForm()
+    # Validate form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template("name.html",
+                           name=name,
+                           form=form)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+
